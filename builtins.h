d437b880ae8300bbfb5ff452c4d517ebebeac6d4
Murathan Elcuman <murathanelcuman@Murathan-MBP.home>
2024-09-09 13:09:58 +0300

    study

---
+++ builtins.h
@@ -0,0 +1,6 @@
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+int builtin_commands(int argc, char **argv);
+
+#endif /* BUILTINS_H */
---
+++ jobs.h
@@ -0,0 +1,27 @@
+#ifndef JOBS_H
+#define JOBS_H
+
+#include "csapp.h"
+
+typedef struct Job {
+    unsigned jid;
+    pid_t pid;
+    int completed;
+    int stopped;
+    int notified;
+    struct Job *last;
+    struct Job *next;
+    char cmdline[MAXLINE];
+} Job;
+
+Job *make_job(unsigned jid, pid_t pid, char *cmdline);
+void remove_job(Job *job);
+Job *job_with_jid(unsigned jid);
+Job *job_with_pid(pid_t pid);
+int mark_process_status(pid_t pid, int status);
+void update_jobs();
+void wait_for_job(Job *j);
+
+extern Job *first_job;
+
+#endif
